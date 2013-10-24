410c75958f3e1a258b75c2b4b04a64b40eccf0a8
John Frimmel <jrfrimme@unca.edu>
2013-10-24 17:15:01 +0000

    updates to modules

--- html/sites/all/modules/geofield/modules/geofield_map/js/geofield_map.js
+++ html/sites/all/modules/geofield/modules/geofield_map/js/geofield_map.js
@@ -1,23 +1,25 @@
 (function ($) {
   Drupal.behaviors.geofieldMap = {
-    attach: function(context) {
-      var settings = Drupal.settings.geofieldMap;
+    attach: function(context, settings) {
+      Drupal.geoField = Drupal.geoField || {};
+      Drupal.geoField.maps = Drupal.geoField.maps || {};
 
-      $('.geofieldMap:not(.processed)').each(function(index, element) {
+      $('.geofieldMap', context).once('geofield-processed', function(index, element) {
         var data = undefined;
         var map_settings = [];
         var pointCount = 0;
-        for (var i in settings) {
-          if (settings[i].map_id == $(element).attr('id')) {
-            data = settings[i].data;
-            map_settings = settings[i].map_settings;
-            break;
-          }
+        var resetZoom = true;
+        var elemID = $(element).attr('id');
+
+        if(settings.geofieldMap[elemID]) {
+            data = settings.geofieldMap[elemID].data;
+            map_settings = settings.geofieldMap[elemID].map_settings;
         }
 
-        if (data != undefined) {
-          var markers = [];
-
+        // Checking to see if google variable exists. We need this b/c views breaks this sometimes. Probably
+        // an AJAX/external javascript bug in core or something.
+        if (typeof google != 'undefined' && typeof google.maps.ZoomControlStyle != 'undefined' && data != undefined) {
+          var features = GeoJSON(data);
           // controltype
           var controltype = map_settings.controltype;
           if (controltype == 'default') { controltype = google.maps.ZoomControlStyle.DEFAULT; }
@@ -58,7 +60,10 @@
             scaleControlOptions: {style: google.maps.ScaleControlStyle.DEFAULT}
           };
 
-          var map = new google.maps.Map(document.getElementById($(element).attr('id')), myOptions);
+          var map = new google.maps.Map($(element).get(0), myOptions);
+          // Store a reference to the map object so other code can interact
+          // with it.
+          Drupal.geoField.maps[elemID] = map;
 
           var range = new google.maps.LatLngBounds();
 
@@ -66,76 +71,57 @@
             content: ''
           });
 
-          for (var i in data) {
-            switch (data[i].type) {
-              case 'point':
-                var point = new google.maps.LatLng(data[i].points[0]['lat'], data[i].points[0]['lon']);
-                range.extend(point);
-                pointCount++;
-
-                var marker = new google.maps.Marker({
-                  position: point,
-                  map: map,
-                  title: "test"
-                });
-
-                if (data[i].icon != undefined) {
-                  marker.setIcon(data[i].icon);
+          if (features.setMap) {
+            placeFeature(features, map, range);
+            // Don't move the default zoom if we're only displaying one point.
+            if (features.getPosition) {
+              resetZoom = false;
+            }
+          } else {
+            for (var i in features) {
+              if (features[i].setMap) {
+                placeFeature(features[i], map, range);
+              } else {
+                for (var j in features[i]) {
+                  if (features[i][j].setMap) {
+                    placeFeature(features[i][j], map, range);
+                  }
                 }
-                marker.setValues({'data_id': i});
-
-                if (data[i].points[0]['text'] !== '') {
-                  google.maps.event.addListener(marker, 'click', function() {
-                    if (data[this.data_id].points[0].text) {
-                      infowindow.setContent(data[this.data_id].points[0].text);
-                      infowindow.open(map, this);
-                    }
-                  });
-                }
-
-              break;
-              case 'linestring':
-                var linestring = [];
-                for (var j in data[i].points) {
-                  var point = new google.maps.LatLng(data[i].points[j]['lat'], data[i].points[j]['lon']);
-                  range.extend(point);
-                  pointCount++;
-                  linestring.push(point);
-                }
-                var linestringObject = new google.maps.Polyline({
-                  path: linestring
-                });
-
-                linestringObject.setMap(map);
-              break;
-              case 'polygon':
-                var polygon = [];
-                for (var j in data[i].points) {
-                  var point = new google.maps.LatLng(data[i].points[j]['lat'], data[i].points[j]['lon']);
-                  range.extend(point);
-                  pointCount++;
-                  polygon.push(point);
-                }
-                var polygonObject = new google.maps.Polygon({
-                  paths: polygon
-                });
-
-                polygonObject.setMap(map);
-              break;
+              }
             }
           }
 
-          if (pointCount == 0) {
-
-          }
-          else if (pointCount > 1) {
+          if (resetZoom) {
             map.fitBounds(range);
           } else {
             map.setCenter(range.getCenter());
           }
         }
+        
+        function placeFeature(feature, map, range) {
+          var properties = feature.get('geojsonProperties');
+          if (feature.setTitle && properties && properties.title) {
+            feature.setTitle(properties.title);
+          }
+          feature.setMap(map);
+          if (feature.getPosition) {
+            range.extend(feature.getPosition());
+          } else {
+            var path = feature.getPath();
+            path.forEach(function(element) {
+              range.extend(element);
+            });
+          }
 
-        $(element).addClass('processed');
+          if (properties && properties.description) {
+            var bounds = feature.get('bounds');
+            google.maps.event.addListener(feature, 'click', function() {
+              infowindow.setPosition(bounds.getCenter());
+              infowindow.setContent(properties.description);
+              infowindow.open(map);
+            });
+          }
+        }
       });
     }
   }
